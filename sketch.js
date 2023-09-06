7d16535bc05871bbebaf26d5f72dabd3a1b3bf45
tulhoo <142426070+tulhoo@users.noreply.github.com>
2023-09-06 11:52:15 -0300

    adicionando arquivo css e js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(1000, 1000);
+    background("white");
+  }
+    
+  function draw() {
+    stroke("#000000");
+    fill("rgb(255,255,255)"); 
+    
+    
+    if(mouseIsPressed) {
+    rect(mouseX, mouseY, 40000000000, 35,);
+  13
+   }, 40, 35,);
+   }
+  }
